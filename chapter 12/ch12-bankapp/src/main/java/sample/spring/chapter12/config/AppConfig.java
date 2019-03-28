f57e7ad509359e1edb8d9107750e54310fe675c0
helen <hhma@alumni.cpp.edu>
2019-03-27 21:58:51 -0400

    replace web.xml config with Java code

---
+++ chapter 12/ch12-bankapp/src/main/java/sample/spring/chapter12/config/AppConfig.java
@@ -0,0 +1,10 @@
+package sample.spring.chapter12.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(basePackages="sample.spring.chapter12")
+public class AppConfig {
+}
---
+++ chapter 12/ch12-bankapp/src/main/java/sample/spring/chapter12/config/WebConfig.java
@@ -0,0 +1,14 @@
+package sample.spring.chapter12.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+@EnableWebMvc
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
