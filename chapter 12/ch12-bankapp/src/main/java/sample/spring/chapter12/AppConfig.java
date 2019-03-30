0d349119d763db6fd95006eefeb4ad4151014d50
helen <hhma@alumni.cpp.edu>
2019-03-29 21:14:04 -0400

    add excluded filter

--- chapter 12/ch12-bankapp/src/main/java/sample/spring/chapter12/AppConfig.java
+++ chapter 12/ch12-bankapp/src/main/java/sample/spring/chapter12/AppConfig.java
@@ -2,9 +2,13 @@ package sample.spring.chapter12;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import sample.spring.chapter12.web.FixedDepositController;
 
 
 @Configuration
-@ComponentScan(basePackages="sample.spring.chapter12")
+@ComponentScan(basePackages="sample.spring.chapter12",
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE,
+                value= FixedDepositController.class))
 public class AppConfig {
 }
